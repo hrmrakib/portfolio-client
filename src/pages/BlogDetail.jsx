42dcf4b763f4c6bdd919b5edbaa89bcd9ee8a5de
hrmrakib <hrmrakibs@gmail.com>
2024-06-22 14:15:28 +0600

    responsive the blog details

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -14,13 +14,15 @@ const BlogDetail = () => {
       >
         <IoHomeOutline className='text-2xl text-purple-600' />
       </Link>
-      <div className='w-[70%] mx-auto pt-20'>
-        <img className='rounded w-full h-96' src={blog?.image} alt='' />
+      <div className='w-[94%] lg:w-[70%] mx-auto pt-20'>
+        <img className='rounded w-full h-48 lg:h-96' src={blog?.image} alt='' />
 
         {/* title */}
-        <h2 className='text-4xl text-gray-200 my-5'>{blog?.headingTitle}</h2>
+        <h2 className='text-2xl lg:text-4xl text-gray-200 my-5'>
+          {blog?.headingTitle}
+        </h2>
 
-        <p className='text-xl text-gray-300 my-5'>{blog?.summery}:</p>
+        <p className='lg:text-xl text-gray-300 my-5'>{blog?.summery}:</p>
 
         <ul className='py-7'>
           {blog?.points?.map((point, i) => (
